ae4110d4e4b01ac42603dec3e19b44ef823214fd
g8rdier <g.kobilarov@purpose.hr>
2025-07-07 03:33:42 +0200

    docs: add ExampleRepeat function to document Repeat usage

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 3)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3)
+	fmt.Println(repeated)
+	// Output: aaa
 }
