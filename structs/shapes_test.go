4dd001d755b73c2568735426f6fbd857bb6a99e5
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 01:29:46 +0200

    refactor: duplicate rectangle test

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,16 +2,6 @@ package structs
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := rectangle.Area()
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
 
 	t.Run("rectangles", func(t *testing.T) {
