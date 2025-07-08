e8b5e0142f693fdab2ca92f53ba6de7d02346d79
g8rdier <g.kobilarov@purpose.hr>
2025-07-08 18:06:36 +0200

    test: fix TestSum to expect integer result from Sum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -9,7 +9,7 @@ func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
-	want := []int{3, 9}
+	want := 15
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
