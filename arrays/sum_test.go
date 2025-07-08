7d9a337402bdf7b8936202d87c595f76037ac9c2
g8rdier <g.kobilarov@purpose.hr>
2025-07-08 17:23:57 +0200

    test: revert TestSumAll to correct type-safe slice comparison

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -9,7 +9,7 @@ func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
-	want := 15
+	want := []int{3, 9}
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
