1da9cecd5b30b080587fd862894591c44a221722
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 06:32:27 +0200

    feat: add minimal Add method implementation returning nil error

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("cannot add word because it already exists")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,5 +15,7 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
+	d[word] = definition
+	return nil
 }
