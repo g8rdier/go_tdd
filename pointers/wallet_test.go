a2ac7595d9270ff036b3ecdf768f9370585d0b23
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 04:26:43 +0200

    test: update assertError helper to check specific error messages in Wallet tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -35,10 +35,14 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, startingBalance)
 	})
 
-	assertError := func(t testing.TB, err error) {
+	assertError := func(t testing.TB, got error, want string) {
 		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
+
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
 		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
 	}
 }
