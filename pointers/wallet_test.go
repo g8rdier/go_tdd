3e76d01b6b356af9ed05acf374fefc618d9e884f
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 08:11:49 +0200

    feat: implement error handling in Withdraw method for insufficient funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -16,12 +16,36 @@ func TestWallet(t *testing.T) {
 		}
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
+		if err != nil {
+			t.Errorf("got an error but didn't want one")
+		}
 
 		got := wallet.Balance()
 		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		if err == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if err != ErrInsufficientFunds {
+			t.Errorf("got %q, want %q", err, ErrInsufficientFunds)
+		}
+
+		got := wallet.Balance()
+		want := Bitcoin(20)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
