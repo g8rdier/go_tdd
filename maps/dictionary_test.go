d35066d3a68a192a36f3d32a03254cbd654463d4
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 06:48:36 +0200

    test: add TestUpdate with error handling for existing and non-existing words

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,25 +22,27 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	t.Run("new word", func(t *testing.T) {
-		dictionary := Dictionary{}
-		word := "test"
-		definition := "this is just a test"
-
-		err := dictionary.Add(word, definition)
-
-		assertError(t, err, nil)
-		assertDefinition(t, dictionary, word, definition)
-	})
 
 	t.Run("existing word", func(t *testing.T) {
 		word := "test"
 		definition := "this is just a test"
 		dictionary := Dictionary{word: definition}
-		err := dictionary.Add(word, "new definition")
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
 
 		assertError(t, err, ErrWordExists)
-		assertDefinition(t, dictionary, word, definition)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
 func TestUpdate(t *testing.T) {
