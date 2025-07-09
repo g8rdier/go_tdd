1e40de2f7eee3df962ca94b4ceec16382d6048bf
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 15:48:13 +0200

    feat: add 1 second delay in Countdown loop using time.Sleep

--- mocking/main.go
+++ mocking/main.go
@@ -12,8 +12,9 @@ const countdownStart = 3
 func Countdown (out io.Writer) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
