ee617fb7e5c0519258727e9be83fc54113421aa8
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 11:48:55 +0200

    feat: add HTTP server and handler using Greet with http.ResponseWriter

--- di/di.go
+++ di/di.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"net/http"
 )
 
 func Greet(writer io.writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
