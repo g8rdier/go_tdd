e929dec27e8800e52ff396a5ef674638cefb23e4
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 08:07:41 +0200

    test: add Deposit call to Wallet test to verify deposit functionality

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,9 +4,10 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
+	wallet.Deposit(10)
 
 	got := wallet.Balance()
-	want := 0
+	want := 10
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
