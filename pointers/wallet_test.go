07b778fdb75f3dca638426b841a41a45175d32a4
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 08:12:06 +0200

    refactor: extract test helper functions to reduce duplication in Wallet tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -8,47 +8,49 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(10))
 
-		if err != nil {
-			t.Errorf("got an error but didn't want one")
-		}
-
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		if err == nil {
-			t.Fatal("didn't get an error but wanted one")
-		}
-
-		if err != ErrInsufficientFunds {
-			t.Errorf("got %q, want %q", err, ErrInsufficientFunds)
-		}
-
-		got := wallet.Balance()
-		want := Bitcoin(20)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(20))
 	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Errorf("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
