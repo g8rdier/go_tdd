3bdaa7a5f30fd9cb99ecacad240782725b297e13
g8rdier <g.kobilarov@purpose.hr>
2025-07-09 16:32:37 +0200

    test: add SpySleeper mock to record Sleep calls for testing Countdown

--- mocking/main.go
+++ mocking/main.go
@@ -14,6 +14,14 @@ type Sleeper interface {
 	Sleep()
 }
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown (out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
