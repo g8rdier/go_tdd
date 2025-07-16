1c8cdaeccbabf4c420329d1ff322a473f0c67313
g8rdier <g.kobilarov@purpose.hr>
2025-07-16 15:12:53 +0200

    refactor: add NewCounter constructor and update tests to avoid copying Mutex

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -7,21 +7,17 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
-
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
 	})
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
