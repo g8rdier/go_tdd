34fa319468d8a666115d7c17022243a432ffa4c2
g8rdier <g.kobilarov@purpose.hr>
2025-07-22 18:54:10 +0200

    refactor: decouple Roman numeral data from behavior

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -2,27 +2,27 @@ package v1
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic >= 10:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic >= 9:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic >= 5:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic >= 4:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic -= 1
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
