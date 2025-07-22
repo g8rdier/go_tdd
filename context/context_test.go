ff504d96b0b463250331c08e4d47d8ce0ded990d
g8rdier <g.kobilarov@purpose.hr>
2025-07-22 13:50:45 +0200

    test: update tests to use SpyStore assertion methods and pass testing.T

--- context/context_test.go
+++ context/context_test.go
@@ -1,6 +1,7 @@
 package context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,33 +11,28 @@ import (
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
 }
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
 }
 
 func TestServer(t *testing.T) {
 	data := "hello, world"
-	svr := Server(&SpyStore{response: data})
-
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	svr.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
 
 	t.Run("returns data from store", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -48,8 +44,23 @@ func TestServer(t *testing.T) {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
 
-		if store.cancelled {
-			t.Errorf("store was cancelled")
-		}
+		store.assertWasNotCancelled()
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		store.assertWasCancelled()
 	})
 }
