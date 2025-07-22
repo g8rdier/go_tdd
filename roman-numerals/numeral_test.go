5203c8b68eea5ae32cbcdf8d7786f75c1071fc70
g8rdier <g.kobilarov@purpose.hr>
2025-07-22 16:40:21 +0200

    test: add subtests for ConvertToRoman including 1->I and 2->II

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,22 @@ package v1
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("converts 1 to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("converts 2 to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
