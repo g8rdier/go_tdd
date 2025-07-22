e878fa0d9bdaca6a9359e6b2ed88368784924db2
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 00:57:11 +0200

    test: add property-based test for round-trip Arabic-Roman conversion

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 // Move `cases` here as a package-level variable
@@ -60,5 +61,17 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic int) bool {
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
