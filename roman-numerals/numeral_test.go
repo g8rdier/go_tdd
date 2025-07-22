76466ac1a49799ea8e5db5e60b73c13105d36c66
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 01:06:05 +0200

    test: update test cases and property test to use uint16

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -8,7 +8,7 @@ import (
 
 // Move `cases` here as a package-level variable
 var cases = []struct {
-	Arabic int
+	Arabic uint16
 	Roman  string
 }{
 	{Arabic: 1, Roman: "I"},
@@ -65,10 +65,11 @@ func TestConvertingToArabic(t *testing.T) {
 }
 
 func TestPropertiesOfConversion(t *testing.T) {
-	assertion := func(arabic int) bool {
-		if arabic < 0 || arabic > 3999 {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
 			return true
 		}
+		t.Log("testing", arabic)
 		roman := ConvertToRoman(arabic)
 		fromRoman := ConvertToArabic(roman)
 		return fromRoman == arabic
