837aafc9aeee22d2f7f4f15896d948d6dc38ebf0
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 03:21:32 +0200

    feat: implement SVGWriter with XML output and consolidate constants in svgwriter.go

--- maths/clockface/clockface.go
+++ maths/clockface/clockface.go
@@ -10,17 +10,11 @@ type Point struct {
 	Y float64
 }
 
-const (
-	secondHandLength = 90
-	clockCenterX     = 150
-	clockCenterY     = 150
-)
-
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
 	p = Point{p.X, -p.Y}
-	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
 	return p
 }
 
