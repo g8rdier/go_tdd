6993a68bba185211127d8dede38cce1476fc6fec
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 03:28:25 +0200

    test: update SVG coordinate expectations to match three-decimal precision formatting

--- maths/clockface/clockface_acceptance_test.go
+++ maths/clockface/clockface_acceptance_test.go
@@ -1,7 +1,7 @@
 package clockface_test
 
 import (
-	"bytes" // Changed from strings
+	"bytes"
 	"encoding/xml"
 	"testing"
 	"time"
@@ -12,22 +12,21 @@ import (
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	b := bytes.Buffer{} // Changed from strings.Builder
+	b := bytes.Buffer{}
 	clockface.SVGWriter(&b, tm)
 
 	svg := Svg{}
-	xml.Unmarshal(b.Bytes(), &svg) // b.Bytes() instead of []byte(b.String())
+	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := "150" // Define the expected values
-	y2 := "60"
+	x2 := "150.000"
+	y2 := "60.000"
 
 	for _, line := range svg.Line {
 		if line.X2 == x2 && line.Y2 == y2 {
-			return // Test passes if we find the expected line
+			return
 		}
 	}
 
-	// Fixed error message with proper variables
 	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
 
