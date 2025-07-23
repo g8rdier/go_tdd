4d8441c07458e182d5876dccf2521b67ded2b585
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 04:09:01 +0200

    test: add TestMinuteHandPoint expecting unit vector Point{0, -1} at 30 minutes

--- maths/clockface/clockface_test.go
+++ maths/clockface/clockface_test.go
@@ -80,3 +80,21 @@ func TestMinutesInRadians(t *testing.T) {
 		})
 	}
 }
+
+func TestMinuteHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 30, 0), Point{0, -1}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minuteHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v, but got %v", c.point, got)
+			}
+		})
+	}
+}
