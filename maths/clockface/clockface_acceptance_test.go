16930a23a0425eb524caeea27d57a7f12f76fde4
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 03:48:56 +0200

    test: add TestSVGWriterMinuteHand expecting minute hand at position Y2:70

--- maths/clockface/clockface_acceptance_test.go
+++ maths/clockface/clockface_acceptance_test.go
@@ -80,3 +80,29 @@ func simpleTime(hours, minutes, seconds int) time.Time {
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
 }
+
+func TestSVGWriterMinuteHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
