3e1519d2e550d3caab601e4591228024d8dadc12
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 03:09:45 +0200

    test: add TestSVGWriterAtMidnight with XML parsing for SVG output validation

--- maths/clockface/clockface_acceptance_test.go
+++ maths/clockface/clockface_acceptance_test.go
@@ -1,21 +1,30 @@
 package clockface_test
 
 import (
+	"encoding/xml"
+	"strings"
 	"testing"
 	"time"
 
 	"example.com/hello/maths/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
+func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	var b strings.Builder
+	clockface.SVGWriter(&b, tm)
 
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	svg := Svg{}
+	xml.Unmarshal([]byte(b.String()), &svg)
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
 	}
+
+	t.Errorf("Expected to find the second hand %v, in SVG got %v", want, got)
 }
 
 func TestSecondHandAt30Seconds(t *testing.T) {
@@ -27,4 +36,31 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
 	}
+}
+
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+
+	Circle struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
