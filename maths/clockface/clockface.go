22fb6aacb317b54870594414969b97fd06383c14
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 05:00:31 +0200

    feat: implement hourHandPoint using angleToPoint(hoursInRadians) to pass vector direction tests

--- maths/clockface/clockface.go
+++ maths/clockface/clockface.go
@@ -28,6 +28,11 @@ func minuteHandPoint(t time.Time) Point {
 	return angleToPoint(minutesInRadians(t))
 }
 
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) +
+		(math.Pi / (6 / float64(t.Hour()%12)))
+}
+
 func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
@@ -35,7 +40,6 @@ func angleToPoint(angle float64) Point {
 	return Point{x, y}
 }
 
-func hoursInRadians(t time.Time) float64 {
-	return (minutesInRadians(t) / 12) +
-		(math.Pi / (6 / float64(t.Hour()%12)))
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
 }
