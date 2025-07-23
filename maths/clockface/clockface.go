dc2bc43c32ad60815a1f2836c4597144985a51e7
g8rdier <g.kobilarov@purpose.hr>
2025-07-23 04:23:49 +0200

    feat: implement minuteHand function and constant to pass acceptance test

--- maths/clockface/clockface.go
+++ maths/clockface/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -22,25 +24,16 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
 
-func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-
-	return Point{x, y}
+func minutesInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / float64(t.Minute())))
 }
 
-func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) +
-		(math.Pi / (30 / float64(t.Minute())))
+func secondHandPoint(t time.Time) Point {
+	return angleToPoint(secondsInRadians(t))
 }
 
 func minuteHandPoint(t time.Time) Point {
-	angle := minutesInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-
-	return Point{x, y}
+	return angleToPoint(minutesInRadians(t))
 }
 
 func angleToPoint(angle float64) Point {
@@ -48,4 +41,14 @@ func angleToPoint(angle float64) Point {
 	y := math.Cos(angle)
 
 	return Point{x, y}
+}
+
+const minuteHandLength = 80
+
+func minuteHand(w io.Writer, t time.Time) {
+	p := minuteHandPoint(t)
+	p = Point{p.X * minuteHandLength, p.Y * minuteHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:2px;"/>`, p.X, p.Y)
 }
